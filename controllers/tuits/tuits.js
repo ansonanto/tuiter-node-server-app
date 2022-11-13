1e6c578b20d35c302232f0f8c69340a65860e324
Anson Antony <ansonanto53@gmail.com>
2022-11-12 23:18:56 -0500

    trial

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -11,8 +11,8 @@ export default [
      "_id": 234, "topic": "Traffic", "username": "The Boring Company",
      "handle": "@boringcompany", "time": "2h", "image": "boring2.png",
      "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "likes": 2345, "dislikes": 200,
+     "tuit": "The Anson Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+     "liked": true, "likes": 2222, "dislikes": 200,
      "replies": 123, "retuits": 432
     },
     ];
